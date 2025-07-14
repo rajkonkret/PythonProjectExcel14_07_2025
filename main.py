26069aafdbd0595b0b0c0d4453835c50eb791bae
rajkonkret <rajkonkret660@gmail.com>
2025-07-14 09:54:42 +0200

    [1 1] init

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+# 2 x shift - podręczna wyszukiwarka
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# (.venv) PS C:\Users\Szkolenie\PycharmProjects\PythonProjectExcel14_07_2025> python .\main.py
+# Hi, PyCharm
+# wheel, theme
