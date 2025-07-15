c81e80b0fc3585fef38d392244955a01170ca4f9
rajkonkret <rajkonkret660@gmail.com>
2025-07-15 14:36:06 +0200

    [2 19] wykres.py

---
+++ day_2/wykres_!.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({"A": [1, 2, 3, 4], "B": [4, 5, 6, 7]})
+print(df)
+#    A  B
+# 0  1  4
+# 1  2  5
+# 2  3  6
+# 3  4  7
+
+print(df.describe())
+# count  4.000000  4.000000
+# mean   2.500000  5.500000
+# std    1.290994  1.290994
+# min    1.000000  4.000000
+# 25%    1.750000  4.750000
+# 50%    2.500000  5.500000
+# 75%    3.250000  6.250000
+# max    4.000000  7.000000
+
+df.plot.box()
+plt.show()
